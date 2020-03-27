5c8c8beffcfc5c91bb8ea8b9722b77ce94c43c61
IsaiahSanchez <34019869+IsaiahSanchez@users.noreply.github.com>
2020-03-27 00:30:19 -0400

    finished implementing a temp main menu.

---
+++ ZombieBoardGame/Assets/Managers/MainMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void DeleteSave()
+    {
+
+    }
+}
